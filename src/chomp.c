6740fcec47c5ee9c4f1f5a68a9d79075dbab43c4
ferris <jake@fusetools.com>
2015-06-07 06:53:07 +0200

    lots of formatting and yolo

--- src/chomp.c
+++ src/chomp.c
@@ -1,39 +1,48 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *textLayer;
+
+static Layer *render_layer;
 
 static void window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
-    GRect bounds = layer_get_bounds(window_layer);
+    GRect window_bounds = layer_get_bounds(window_layer);
 
-    text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-    text_layer_set_text(text_layer, "chomp!");
-    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-    layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    GRect textBounds =
+	{
+	    .origin = { 0, 72 },
+	    .size = { window_bounds.size.w, 20 }
+	};
+    textLayer = text_layer_create(textBounds);
+    text_layer_set_text(textLayer, "chomp!");
+    text_layer_set_text_alignment(textLayer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(textLayer));
 }
 
 static void window_unload(Window *window) {
-    text_layer_destroy(text_layer);
+    text_layer_destroy(textLayer);
 }
 
-static void init(void) {
+static void init() {
     window = window_create();
-    window_set_window_handlers(window, (WindowHandlers) {
+    WindowHandlers windowHandlers =
+	{
 	    .load = window_load,
-	    .unload = window_unload,
-    });
-    const bool animated = true;
-    window_stack_push(window, animated);
+	    .unload = window_unload
+	};
+    window_set_window_handlers(window, windowHandlers);
+    window_stack_push(window, true);
 }
 
-static void deinit(void) {
+static void deinit() {
     window_destroy(window);
 }
 
-int main(void) {
+int main() {
     init();
 
     app_event_loop();
+
     deinit();
 }
